1fb9230a3372ab904cf0a805cc87042e26d6eefa
Paul Cunningham <31044139+ugufru@users.noreply.github.com>
2020-12-04 00:17:44 -0500

    2nd revision.

--- NeoClockGemmaM0.ino
+++ NeoClockGemmaM0.ino
@@ -1,11 +1,17 @@
 #include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include <TimeLib.h>
+#include <DS1307RTC.h>
 
-#define LED_PIN    1
+#define LED_PIN    2
 #define LED_COUNT 12
 
+tmElements_t tm;
 
-// Declare our NeoPixel strip object:
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+int hh = 0;
+int mm = 0;
+int ss = 0;
+
 // Argument 1 = Number of pixels in NeoPixel strip
 // Argument 2 = Arduino pin number (most are valid)
 // Argument 3 = Pixel type flags, add together as needed:
@@ -14,51 +20,62 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+byte hue = 0;
+byte offset = 4;
+byte offset2 = 3;
 
-
-int hour = 0;
-int minute = 0;
-int second = 0;
 
 
 void setup()
 {
+  Serial.begin(9600);
+  while (!Serial) ; // wait for serial
+
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+  strip.setBrightness(100); // Set BRIGHTNESS to about 1/5 (max = 255)
+
+  RTC.get();
+
+  if (RTC.chipPresent())
+  {
+    flashColor(Wheel(0));
+  }
+  else
+  {
+    flashColor(Wheel(80));
+  }
 }
 
 
 void loop()
 {
-  second++;
-
-  if (second > 59)
-  {
-    second = 0;
-    minute++;
-  }
-
-  if (minute > 59)
-  {
-    minute = 0;
-    hour ++;
-  }
-
-  if (hour > 11)
-  {
-    hour = 0;
-  }
-
   updateTime();
-  delay(1000);
+  delay(250);
 }
 
 void updateTime()
 {
-  int h = hour;
-  int m = minute / 5;
-  int s = second / 5;
+  RTC.read(tm);
+  
+  int h = (tm.Hour > 12) ? tm.Hour - 12 : tm.Hour ;
+  int m = tm.Minute / 5;
+  int s = tm.Second / 5;
+
+  Serial.print("tm.Hour = ");
+  Serial.print(tm.Hour);
+  Serial.print(", tm.Minute = ");
+  Serial.print(tm.Minute);
+  Serial.print(", tm.Second = ");
+  Serial.print(tm.Second);
+
+  Serial.print(", h = ");
+  Serial.print(h);
+  Serial.print(", m = ");
+  Serial.print(m);
+  Serial.print(", s = ");
+  Serial.println(s);
 
   for (int i = 0; i < 12; i++)
   {
@@ -76,19 +93,83 @@ void updateTime()
     }
     else
     {
-      if (i == 0 || i == 3 || i == 6 || i == 9)
+      if (i == 0)
+      {
+        strip.setPixelColor(i, 100, 100, 0);
+      }
+      else if (i == 3 || i == 6 || i == 9)
       {
         strip.setPixelColor(i, 50, 50, 50);
       }
       else
       {
-        if (second % 2 == 0)
+        if (tm.Second % 2 != 0)
           strip.setPixelColor(i, 0, 0, 0);
         else
-          strip.setPixelColor(i, 10, 10, 10);
+          strip.setPixelColor(i, 50, 50, 50);
       }
     }
   }
 
   strip.show();
+}
+
+void doTheLights()
+{
+  pushPixel(Wheel(hue));
+
+  hue = (hue + offset) % 0xFF;
+
+  offset = (offset + offset2) % 0x40;
+  offset2 = (offset2 + 1) % 0x20;
+}
+
+
+void pushPixel(uint32_t color)
+{
+  uint16_t count = strip.numPixels();
+
+  for (uint16_t i = 0; i < count - 1; i++)
+  {
+    uint32_t c = strip.getPixelColor(i + 1);
+    strip.setPixelColor(i, c);
+  }
+
+  strip.setPixelColor(count - 1, color);
+  strip.show();
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if (WheelPos < 85) {
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
+  else if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  else {
+    WheelPos -= 170;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+void flashColor(uint32_t color)
+{
+  for (int x = 0; x < 5; x++)
+  {
+    for (int i = 0; i < 12; i++)
+    {
+      strip.setPixelColor(i, color);
+    }
+    strip.show();
+    delay(100);
+    for (int i = 0; i < 12; i++)
+    {
+      strip.setPixelColor(i, 0);
+    }
+    strip.show();
+    delay(100);
+  }
 }
